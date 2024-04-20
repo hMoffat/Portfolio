7422910da8aa58c11a691f35cf08908ce554767e
hMoffat <hmoffat.work@gmail.com>
2024-04-20 15:17:42 +0100

    Add fonts, colours and main buttons.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./styles/components/root.scss";
 import { AppContainer } from "./styles/components/App.module.scss";
 
 function App() {
-  return <div className={AppContainer} />;
+  return <div className={AppContainer}></div>;
 }
 
 export default App;
