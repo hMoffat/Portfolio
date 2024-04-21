8ffb6489438473cc0fc88d21a6a9d7d2cf6221ec
hMoffat <hmoffat.work@gmail.com>
2024-04-21 16:09:36 +0100

    Added component and semantic html skelaton.

---
+++ src/components/About/About.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <section>
+      <div className="headerAndImg">
+        <header>
+          <h3>A little about me...</h3>
+          <h4>Thank you for visiting my portfolio</h4>
+        </header>
+        <img></img>
+      </div>
+      <p>text</p>
+    </section>
+  );
+}
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <a>links</a>
+      <button>contact</button>
+      <a>icons8</a>
+    </footer>
+  );
+}
