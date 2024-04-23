9b60a698b94b66bc29966924ffb1abb4b8438a5a
hMoffat <hmoffat.work@gmail.com>
2024-04-23 09:08:40 +0100

    Add style to header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
+import { HeaderContainer, subtitleUnderline } from "./Header.module.scss";
+
 export default function Header() {
   return (
-    <header id="Home">
+    <header id="Home" className={HeaderContainer}>
       <h1>Helen Moffat</h1>
       <h2>Junior Software Developer</h2>
+      <div className={subtitleUnderline} />
     </header>
   );
 }
